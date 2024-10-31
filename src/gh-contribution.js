ecbfa898d47bfb85d35501895653798bedbc7734
Rifat Noor <48450270+rifatNR@users.noreply.github.com>
2024-10-31 15:25:37 +0600

    Show GitHub contribution (Didn't include it with main command) (#6)
    * base github contribution is showing
    
    * fetch real contribution and improve color
    
    * Changed file structure
    
    ---------
    
    Co-authored-by: Faisal <mdfaisal.dev@gmail.com>

---
+++ src/gh-contribution.js
@@ -0,0 +1,56 @@
+import chalk from "chalk";
+import {
+    getOppositeColor,
+    getSampleContribution,
+    getUserContributions,
+} from "./utils/github.js";
+import fs from "fs";
+
+const init = async () => {
+    const contributions = await getUserContributions("rifatNR");
+    // const contributions = await getSampleContribution(
+    //     "contributions-sample.json"
+    // );
+
+    const weeklyContribution =
+        contributions.data.user.contributionsCollection.contributionCalendar
+            .weeks;
+
+    if (!weeklyContribution) {
+        console.log("ERROR: Contribution not found.");
+        return;
+    }
+
+    let formattedWeeklyContribution = {
+        _1: [],
+        _2: [],
+        _3: [],
+        _4: [],
+        _5: [],
+        _6: [],
+        _7: [],
+    };
+
+    weeklyContribution.forEach((item, index) => {
+        const contributionDays = item.contributionDays;
+        formattedWeeklyContribution._1[index] = contributionDays[0];
+        formattedWeeklyContribution._2[index] = contributionDays[1];
+        formattedWeeklyContribution._3[index] = contributionDays[2];
+        formattedWeeklyContribution._4[index] = contributionDays[3];
+        formattedWeeklyContribution._5[index] = contributionDays[4];
+        formattedWeeklyContribution._6[index] = contributionDays[5];
+        formattedWeeklyContribution._7[index] = contributionDays[6];
+    });
+
+    Object.entries(formattedWeeklyContribution).forEach(([key, value]) => {
+        const week = value;
+        let chart = "";
+        week.forEach((day) => {
+            const color = day?.contributionCount > 0 ? day?.color : "#292929";
+            chart = chart + " " + chalk.hex(color ?? "#00000000")("■");
+        });
+        console.log(chart);
+    });
+};
+
+init();
---
+++ src/utils/github.js
@@ -0,0 +1,58 @@
+import "dotenv/config";
+import fs from "fs";
+import Color from "color";
+
+export const getUserContributions = async (username) => {
+    const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+
+    const query = `
+      query($username: String!) {
+        user(login: $username) {
+          contributionsCollection {
+            contributionCalendar {
+              totalContributions
+              weeks {
+                contributionDays {
+                  date
+                  contributionCount
+                  color
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const variables = { username };
+
+    const response = await fetch("https://api.github.com/graphql", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${GITHUB_ACCESS_TOKEN}`,
+        },
+        body: JSON.stringify({ query, variables }),
+    });
+
+    const data = await response.json();
+    // console.dir(data, { depth: null });
+    // fs.writeFileSync("contributions-sample.json", JSON.stringify(data));
+    return data;
+};
+
+export const getSampleContribution = async (filePath) => {
+    try {
+        const data = fs.readFileSync(filePath, "utf-8");
+        const jsonData = JSON.parse(data);
+        return jsonData;
+    } catch (err) {
+        console.error("Error reading settings JSON file: ", err);
+        return null;
+    }
+};
+
+export const getOppositeColor = (hex) => {
+    const color = Color(hex);
+    return color.isLight() ? color.darken(0.5).hex() : color.lighten(0.5).hex();
+};
